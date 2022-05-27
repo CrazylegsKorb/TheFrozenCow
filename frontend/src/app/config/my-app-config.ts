68c930acc015daed460d71d8924d2bc4223bc72e
Spencer <spencerkorb@gmail.com>
2022-05-27 10:05:00 -0400

    .

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc:{
-        clientId: '0oa51aby97QNlR9kJ5d7',
-        issuer: 'https://dev-34862651.okta.com/oauth2/default',
+        clientId: '0oa570gxwunOb8WGq5d7',
+        issuer: 'https://dev-4455427.okta.com/oauth2/default',
         redirectUri: 'https://thefrozencow.azurewebsites.net/login/callback',
         scopes: ['openid','profile','email']
     }
